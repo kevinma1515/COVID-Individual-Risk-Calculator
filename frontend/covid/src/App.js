e7d302ffbab16ce630d13ee03241e86d22ae6de3
Timothy Gan <timothygan@utexas.edu>
2021-01-16 11:14:13 -0600

    added frontend

---
+++ frontend/covid/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import FullCalculatorView from './components/FullCalculatorView.js';
+function App() {
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
+}
+
+export default App;
