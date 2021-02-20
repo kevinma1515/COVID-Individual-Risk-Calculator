0f3773d6779adb2d294c890bfc430ea3fb3f1bb7
Loba <ajidevelop@gmail.com>
2021-02-20 12:52:45 -0600

    App created in `app_config.py` Unnecessary declaration of `state_county_json` and `symptom_prediction_model_json` Removed unnecessary white space

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,15 @@
-from flask import Flask, request, abort, jsonify, make_response
+from flask import request, jsonify, make_response
 from flask_cors import cross_origin
-import ProbabilityOfCOVID 
+from app_config import app
+import ProbabilityOfCOVID
 import covidAgeToProbability
 import CovidAGE
 import json
 import pandas as pd
 
-app = Flask(__name__)
-df = pd.read_csv('us-counties.csv') # data collected from NYTimes github
-hf = pd.read_csv('co-est2019-alldata.csv', encoding = "ISO-8859-1") # data collected from US census
-df = df.sort_values(by = ['county', 'state'])
-state_county_json = {}
-symptom_prediction_model_json = {}
+df = pd.read_csv('us-counties.csv')  # data collected from NYTimes github
+hf = pd.read_csv('co-est2019-alldata.csv', encoding="ISO-8859-1")  # data collected from US census
+df = df.sort_values(by=['county', 'state'])
 
 with open('question_jsons/symptom_prediction_model.json', 'r') as f:
     symptom_prediction_model_questions_json = json.load(f)
@@ -39,11 +37,13 @@ def symptom_prediction_model_questions():
     if request.method == 'GET':
         return make_response(symptom_prediction_model_questions_json, 200)
 
+
 @app.route('/covid_age_model_questions', methods=['GET'])
 @cross_origin()
 def covid_age_model_questions():
     if request.method == 'GET':
         return make_response(covid_age_questions_json, 200)
+
 
 @app.route('/state_county_list', methods=['GET'])
 @cross_origin()
@@ -56,11 +56,11 @@ def state_county_list():
 @cross_origin()
 def symptom_prediction_model():
     if request.method == 'POST':
-        data = request.get_json()    
+        data = request.get_json()
         if len(data['age']) == 0:
             data['age'] = 0
-        result = ProbabilityOfCOVID.symptomPredictionModel(float(data['age']), 
-                                                        data['sex'], 
+        result = ProbabilityOfCOVID.symptomPredictionModel(float(data['age']),
+                                                        data['sex'],
                                                         data['smellTasteSymptom'],
                                                         data['coughSymptoms'],
                                                         data['severeFatigues'],
@@ -71,19 +71,19 @@ def symptom_prediction_model():
 
 @app.route('/community_risk_result', methods=['POST'])
 @cross_origin()
-def community_risk():    
+def community_risk():
     if request.method == 'POST':
-        data = request.get_json()    
+        data = request.get_json()
         state_name = data['state']
         county_name = data['county']
-                
+
         # narrowing the rows to just the state and county desired
         df_rowValue = df.loc[(df['state']== state_name) & (df['county'] == county_name)]
         # find the row number of a specific county and state for census dataframe
         hf_row = hf['Combined'].str.contains(state_name + county_name,na = False).idxmax()
-        # reverse the dataframe 
+        # reverse the dataframe
         df_rowValue = df_rowValue[::-1]
-        
+
         # locate the county population and county active cases
         county_population = hf.iloc[hf_row, 14]
         print("The population of", state_name + " " + county_name, "County is", county_population, "people.")
@@ -93,7 +93,7 @@ def community_risk():
         print(county_name, "has", county_activeCases, "active cases.")
         # calculate the community risk
         comm_risk = int(county_activeCases)/int(county_population)
-        
+
         return make_response(jsonify({"result" : comm_risk}), 200)
 
 
@@ -101,14 +101,14 @@ def community_risk():
 @cross_origin()
 def covid_age_result():
     if request.method == 'POST':
-        data = request.get_json()    
+        data = request.get_json()
         if len(str(data['age'])) == 0:
             data['age'] = 0
         if len(str(data['bmi'])) == 0:
             data['bmi'] = 0
         print(data)
-        covidAge = CovidAGE.covidAge(int(data['age']), 
-                                        data['gender'], 
+        covidAge = CovidAGE.covidAge(int(data['age']),
+                                        data['gender'],
                                         data['race'],
                                         int(data['bmi']),
                                         data['asthma'],
