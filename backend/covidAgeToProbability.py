11e8f035ff3060cb3a08d18fd0bee6d72ebc225c
Loba <ajidevelop@gmail.com>
2021-02-23 23:18:02 -0600

    Update riskScore

--- backend/covidAgeToProbability.py
+++ backend/covidAgeToProbability.py
@@ -16,9 +16,9 @@ def ageToProbAnalysis():
     df = df[2:]
     x = df['covid_age'].to_numpy()
     y = df['Point_estimate'].to_numpy()
-    
+
     print(curve_fit(lambda t,a,b: a*np.exp(b*t),  x,  y,p0=[21,0.04]))
-    
+
     plt.plot(x,y,'y')
     ts = np.arange(20,85,1)
     plt.plot(ts, 0.0091882*np.exp(0.10334731*ts),'g')
@@ -27,12 +27,12 @@ def ageToProbAnalysis():
     # plt.yscale("log")
     plt.xlabel('covid age')
     plt.ylabel('point estimate of fatality ratio per 1000')
-    
+
     # Therefore, this proves that the best fit curve for an exponentail distribution of this
-    # curve is y = 0.0091882*exp(0.10334731*x) 
+    # curve is y = 0.0091882*exp(0.10334731*x)
     # where y = point estimate of the conversion from covid age to fatality ratio
     # and x = covid age
-    
+
     # from excel sheet, prob of hospitalization is 0.009exp(0.0679*x)
     # note that P(Hosp)*P(ICU|Hosp) = P(ICU) due to P(Hosp|ICU) = 1
     # so P(ICU) = 0.0001*exp(0.0784*x)
@@ -56,12 +56,12 @@ def conversionToRisk(covidAges, totalSusceptibility):
         probHosp = 0.0009*np.exp(0.0679*85)
     else:
         probHosp = 0.0009*np.exp(0.0679*covidAges)
-    
+
     print("community risk: ", totalSusceptibility)
     print("probability of death: ", probDeath)
     print("probability of hospitlization: ", probHosp)
     print("probability of icu: ", probICU)
-    riskScore = totalSusceptibility*(probDeath + probHosp + probICU)/0.0000015
+    riskScore = totalSusceptibility*(probDeath + probHosp + probICU)/0.0000010
     print("risk score: ", riskScore)
     if riskScore >= 100:
         riskScore = 100
